b7fa905b213d7d81acb5996a6940d65d64cc133e
Rainier <rainierereys242@gmail.com>
2024-07-26 16:09:31 -0400

    migraciones hechas

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=100)
     price = models.IntegerField()
     stock = models.IntegerField()
     
@@ -10,7 +10,7 @@ class Order(models.Model):
     date_time = models.DateField(auto_now_add=True)
     
 class OrderDetail(models.Model):
-    order = models.OneToOneField(Order, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
     quantity = models.IntegerField()
-    product = models.ManyToManyField(Product, on_delete=models.CASCADE)
+    product = models.ManyToManyField(Product)
     
