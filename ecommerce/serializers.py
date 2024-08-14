f99f5e14365efb7ba92b9f43c470e1935c8f2d19
Rainier <rainierereys242@gmail.com>
2024-08-14 15:56:07 -0400

    metodo update del order listo con sus respectivas validaciones y resta de stock en los productos

--- ecommerce/serializers.py
+++ ecommerce/serializers.py
@@ -1,6 +1,8 @@
 from ecommerce.models import Product, Order, OrderDetail
-from rest_framework import serializers
+from rest_framework import serializers, status
+from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
+from rest_framework.response import Response
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -48,7 +50,61 @@ class OrderSerializer(serializers.ModelSerializer):
             'details',
             ] 
     
+    def create(self, validated_data):
+        details = validated_data.pop('details', [])
+        order = super().create(validated_data)
         
+        OrderDetail.objects.bulk_create(
+            [
+                OrderDetail(**detail, order=order) for detail in details
+            ]
+        )
+        
+        return order
     
+    def update(self, instance, validated_data):
+        details = validated_data.pop('details')
+        
+        order_details_to_update = []
+        for detail_data in details:
+            # Para obtener el producto
+            product = Product.objects.get(name=detail_data['product']) 
+
+            # Para buscar o crear OrderDetail
+            print(product.stock)
+            order_detail, created = OrderDetail.objects.get_or_create(
+                order=instance,
+                product=product,
+                defaults={
+                    'quantity': detail_data['quantity'],
+                }
+            )
+
+            # Para actualizar los atributos del objeto OrderDetail
+            for attr, value in detail_data.items():
+                # Para evitar actualizar estos campos
+                #si los atributos no son 'product' ni 'order' 
+                # (que no conviene modificarlos), entonces asigna valor nuevo valor
+                # en este caso seria 'quantity'
+                if attr not in ['product', 'order']:  
+                    setattr(order_detail, attr, value)
+
+            if product.stock >= detail_data['quantity']:
+                product.stock -= detail_data['quantity']
+                product.save()
+                order_details_to_update.append(order_detail)
+            else:
+                raise ValidationError({{f'La cantidad solicitada para el {product} es menor a la cantidad que queda disponible'}})
+           
+        # Campos a actualizar
+        fields = ['product', 'quantity']
+        print(order_details_to_update)
+        if not order_details_to_update:
+            raise ValidationError({'No se realizaron cambios en los detalles del pedido.'})
+        OrderDetail.objects.bulk_update(order_details_to_update, fields) 
+        return instance
+        
+        
+        
             
         
